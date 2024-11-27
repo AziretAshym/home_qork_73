641b38413367daa06fa0f08a6a76a7cea08cc7fd
Ashymov Aziret <aziretashym@gmail.com>
2024-11-27 20:26:37 +0600

    EX_1 Made .get request, when adding text to a URl, the server returns the added text as a response.

---
+++ ex_1/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+const port = 8000;
+
+app.get('/', (req, res) => {
+    return res.send('Home page');
+})
+
+app.get('/:word', (req, res) => {
+    return res.send( `${req.params.word} page`);
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Server running on port: http:/localhost:${port}`);
+});
