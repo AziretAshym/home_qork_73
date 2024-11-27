64a3c4c7a8a9f4db740438f1441bdcaab5c8aeac
Ashymov Aziret <aziretashym@gmail.com>
2024-11-27 20:55:30 +0600

    EX_2 Made .get request for decode of the text received after encoded.

--- ex_2/index.js
+++ ex_2/index.js
@@ -14,7 +14,9 @@ app.get('/encode/:password', (req, res) => {
     return res.send(Vigenere.Cipher(password).crypt(req.params.password));
 });
 
-
+app.get('/decode/:password', (req, res) => {
+    return res.send(Vigenere.Decipher(password).crypt(req.params.password));
+});
 
 
 app.listen(port, () => {
